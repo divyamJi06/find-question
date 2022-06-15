bac35a78f48ea95bc301f8e00455532740d21dc1
divyamseek <divyam.gupta@seekaxis.com>
2022-06-15 07:14:49 +0530

    Comments are important

---
+++ Indexing.py
@@ -0,0 +1,12 @@
+#indexing in dictionary is not based on numbers, individual keys are the index.
+#--> beacuse of this reason the keys are supposed to be unique
+
+additive_inverse_dict = {1:-1,2:-2,3:-3,4:-4,5:-5}
+
+print(additive_inverse_dict[1])
+#print(additive_inverse_dict[0]) # if key does not exist it will raise KeyError
+
+#why don't we use numbers as index--> dictionary is unordered
+
+#a large dictionary when printed multiple times will give a different output each time
+
